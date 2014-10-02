9193315fa67af2b7af98db0a54895338b9b2c57c
Linda Ortega <lo2258@barnard.edu>
2014-10-01 21:12:49 -0400

    Adding class from CSS
    Changing CSS element through JS without inline style

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-	console.log("OMG JAVASCRIPT")
+	console.log("OMG JAVASCRIPT");
 
 	// id's of celebs and table rows
 	// .length - action
 	// Prints everything after 
-	 
-	alert($("#celebs tr").length + " elements!")
+
+	// Out of the celebraties, inside table rows without headers.
+	// How many of those are even. 
+	//.css changes css, but don't do this because adds inline style (css) to html document
+
+	$("#celebs tbody tr:even").addClass("zebra");
+
 });
