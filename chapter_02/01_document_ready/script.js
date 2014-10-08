8d7328d3e48f08ffba8384f717ee41ff37aa95ff
Linda Ortega <lo2258@barnard.edu>
2014-10-08 19:57:56 -0400

    Hide/show two buttons
    Made hide button hide disclaimer.
    and show button show disclaimer

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -11,4 +11,13 @@ $(document).ready(function(){
 
 	$("#celebs tbody tr:even").addClass("zebra");
 
+	$('#hideButton').click(function(){
+		$('#disclaimer').hide();
+
+	})
+
+	$('#showButton').click(function(){
+		$('#disclaimer').show();
+
+	})
 });
