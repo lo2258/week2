a3487af9f4e5a7f010e046a97de4f0c594451b23
Linda Ortega <lo2258@barnard.edu>
2014-10-08 20:19:23 -0400

    Added toggle button
    Added toggle button to HTML, JavaScript, using if-statement.

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
 	console.log("OMG JAVASCRIPT");
-
-	// id's of celebs and table rows
-	// .length - action
-	// Prints everything after 
-
-	// Out of the celebraties, inside table rows without headers.
-	// How many of those are even. 
-	//.css changes css, but don't do this because adds inline style (css) to html document
-
 	$("#celebs tbody tr:even").addClass("zebra");
 
-	$('#hideButton').click(function(){
-		$('#disclaimer').hide();
-
-	})
-
-	$('#showButton').click(function(){
-		$('#disclaimer').show();
-
+	// Toggle using if-statement
+	$('#toggleButton').click(function(){
+		if ($('#disclaimer').is(':visible')) {
+			$('#disclaimer').hide();
+		}
+		else {
+			$('#disclaimer').show();
+		}
 	})
 });
